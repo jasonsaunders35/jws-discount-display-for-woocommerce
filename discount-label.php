0ed76c67340fa58017e3cc108df96f111c11c320
Jason Saunders <jasonsaunders35@gmail.com>
2017-07-17 16:12:39 -0700

    1st discount label

---
+++ discount-label.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+* Plugin Name: JWS Discount Display
+* Description: Creates labels for discounted products that display each product's dollar or percent discount
+* Version: 1.0.0
+* Author: Jason William Saunders 
+* License: GPL2
+*/
+define( 'JWS__PLUGIN_DIR', plugin_dir_path( __FILE__ ));
+
