aa5c1a856b5dddca5b7c8c03185117d8866392f6
Jason Saunders <jasonsaunders35@gmail.com>
2017-08-28 11:37:11 -0700

    Update form.php

--- backend/form.php
+++ backend/form.php
@@ -11,9 +11,6 @@ function discountlabel_backend_all_scriptsandstyles(){
     wp_enqueue_script('jws_discountlabel_backend_script');
 }
 
-/*============================================================
- *  Call Color Picker
-==============================================================*/
 add_action( 'admin_enqueue_scripts', 'mw_enqueue_color_picker' );
 function mw_enqueue_color_picker( $hook_suffix ) {
     // first check that $hook_suffix is appropriate for your admin page
@@ -25,10 +22,11 @@ function my_custom_submenu_page_callback() {
     
     $jws_select_array =  getConfigurationOptions();
 
-    /*============================================================
-     *  Assign Option Defaults
-    ==============================================================*/
+    /***************    Assign Defaults    ************************************************/
     $options = get_option( 'discountlabeloptions' );
+	if (! is_array($options)){ 
+		$options = []; 
+	} 
     
     $options['display_style'] = isset($options['display_style']) ? $options['display_style'] : "bubble" ;
     $options['enabled'] = isset($options['enabled']) ? $options['enabled'] : "0" ;
@@ -41,15 +39,14 @@ function my_custom_submenu_page_callback() {
     $options['cssborderStyle'] = isset($options['cssborderStyle']) ? $options['cssborderStyle'] : "dashed" ;
     $options['useInProductDetail'] = isset($options['useInProductDetail']) ? $options['useInProductDetail'] : "1" ;
     $options['previewProduct'] = isset($options['previewProduct']) ? $options['previewProduct'] : wc_get_product_ids_on_sale()[0] ;
+
+    /**************    Set Product for Preview ********************************************/
     
-    /*============================================================
-     *  Set Product For Preview
-    ==============================================================*/
     $product_id = $options['previewProduct'];
     $product = wc_get_product($product_id);
     $title =  esc_html($product->get_title());
-    $regular_price_int = $product->get_regular_price();
-    $sale_price_int = $product->get_sale_price();
+    $regular_price_int = esc_html($product->get_regular_price());
+    $sale_price_int = esc_html($product->get_sale_price());
     
     $dollar_discount = esc_html(floor($regular_price_int - $sale_price_int));
     $percent_discount = esc_html(floor((($regular_price_int - $sale_price_int) / $regular_price_int)*100));
@@ -60,10 +57,8 @@ function my_custom_submenu_page_callback() {
     $img_src = esc_attr(wp_get_attachment_image_src( get_post_thumbnail_id( $product_id ), 'single-post-thumbnail' )[0]);
     
 
-    /*============================================================
-     *  Sort configuration rows by 'order' key
-    ==============================================================*/
-    //comparison function
+    /***************    sort configuration options by 'order' key     **********************/
+    //define a comparison function
     function cmp($a, $b) {
         if ($a['order'] == $b['order']) {
             return 0;
@@ -76,39 +71,32 @@ function my_custom_submenu_page_callback() {
     <script>
         var dollar_discount = '<?php echo $dollar_discount; ?>';
         var percent_discount = '<?php echo $percent_discount; ?>';
-        var currencySymbol = "<?php echo  html_entity_decode(get_woocommerce_currency_symbol());?>";
+        var currencySymbol = "<?php echo  html_entity_decode(get_woocommerce_currency_symbol());?>"
         
-        /*============================================================
-         *  JS configuration for Color Pickers
-        ==============================================================*/
-        
-        // array for CSS color-related configurable properities
         <?php for ($x = 0; $x < count($jws_select_array); $x++): 
             if( stripos($jws_select_array[$x]['slug'], 'color') !== false):
-                $colorCssProperties[] = $jws_select_array[$x]['slug'];
+                $colorProperties[] = $jws_select_array[$x]['slug'];
             endif;
         endfor;
-        
-        // update preview based values returned from color pickers on change
-        for ($x = 0; $x < count($colorCssProperties); $x++):?>
-            var <?php echo $colorCssProperties[$x]; ?>Options = {
+        for ($x = 0; $x < count($colorProperties); $x++):?>
+
+            var <?php echo $colorProperties[$x];?>Options = {
                 change: function(event, ui){
-                    jQueryCssProp = '<?php echo str_replace("css","",$colorCssProperties[$x]);?>';
+                    jQueryCssProp = '<?php echo str_replace("css","",$colorProperties[$x]);?>';
                     jQuery(".jws-discount-label").css( jQueryCssProp, ui.color.toString());
-                }
+                },
             };
         <?php endfor; ?>
     
         jQuery(document).ready(function($){
-            <?php for ($x = 0; $x < count($colorCssProperties); $x++):?>
-                    
-                // Register Color Pickers
-                jQuery('.<?php echo $colorCssProperties[$x]; ?>').wpColorPicker(<?php echo $colorCssProperties[$x];?>Options);
-
-                // update preview based values returned from color pickers on Page Load
-                jQueryCssProp = '<?php echo str_replace("css","",$colorCssProperties[$x]);?>';
-                jQuery(".jws-discount-label").css( jQueryCssProp, jQuery('.<?php echo $colorCssProperties[$x]; ?>').val());
-            <?php endfor; ?>
+        <?php for ($x = 0; $x < count($colorProperties); $x++):?>
+            // setup color pickers
+            jQuery('.<?php echo $colorProperties[$x]; ?>').wpColorPicker(<?php echo $colorProperties[$x];?>Options);
+            
+            //onPageLoad
+            jQueryCssProp = '<?php echo str_replace("css","",$colorProperties[$x]);?>';
+            jQuery(".jws-discount-label").css( jQueryCssProp, jQuery('.<?php echo $colorProperties[$x]; ?>').val());
+        <?php endfor; ?>
         });
     </script>
 
@@ -121,10 +109,8 @@ function my_custom_submenu_page_callback() {
                 <h2><?php echo esc_html__( 'General Settings', 'jwsdiscountlabel' ) ?></h2>
                 <table class="form-table">
                     <?php 
-                    
                     // For the top 'General' Settings
                     for ($x = 0; $x < count($jws_select_array)-2; $x++): 
-                        
                         // If Color Property
                         if( stripos($jws_select_array[$x]['slug'], 'color') === false):?> 
                             <tr valign="top" id="tr-<?php echo $jws_select_array[$x]['slug'];?>">
@@ -138,7 +124,6 @@ function my_custom_submenu_page_callback() {
                                 </td>
                             </tr>
                         <?php 
-                        
                         // Else (Non-Color Property)
                         else: ?> 
                             <tr valign="top" id="tr-<?php echo $jws_select_array[$x]['slug'];?>">
@@ -150,12 +135,10 @@ function my_custom_submenu_page_callback() {
                         <?php endif; ?>
                     <?php endfor; ?>
                 </table>
-                
                 <h2 class='advanced'><?php echo esc_html__( 'Advanced Settings', 'jwsdiscountlabel' ) ?></h2>
                 <table class="form-table">
                     <?php 
-                    
-                    // Last 2 'Advanced' Settings
+                    // FOR last 2 'Advanced' Settings
                     for ($x =  count($jws_select_array)-2; $x < count($jws_select_array); $x++): ?>
                         <tr valign="top" id="tr-<?php echo $jws_select_array[$x]['slug'];?>">
                             <th scope="row"><?php echo esc_html__( $jws_select_array[$x]['name'], 'jwsdiscountlabel' ) ?></th>
@@ -169,9 +152,9 @@ function my_custom_submenu_page_callback() {
                         </tr>
                     <?php endfor; ?>
                 </table>
-                <div class = 'instruction'><span class = 'ast'>*</span>Save Changes to update the preview product.</div>
+                <div class = 'instruction'>*Save Changes to update the preview product.</div>
                 <p class="submit">
-                    <input type="submit" class="button-primary" value="<?php echo esc_html__( 'Save Changes', 'jwsdiscountlabel' ); ?>" />
+                    <input type="submit" class="button-primary" value="<?php _e( 'Save Changes', 'jwsdiscountlabel' ); ?>" />
                 </p>
 
             </form>
