fdddc3a375d45f1c8dfc42ea6591b24b4fe6978e
Jason Saunders <jasonsaunders35@gmail.com>
2017-08-28 17:03:43 -0700

    Merge origin/master
    Conflicts:
    	frontend/js/discount-display-setup.js
    	frontend/setup.php

--- frontend/setup.php
+++ frontend/setup.php
@@ -7,22 +7,21 @@ function discountdisplay_frontend_all_scriptsandstyles() {
 
 add_action( 'wp_footer', 'add_footer_script' );
 function add_footer_script() { ?>
-    <?php if ('1' === get_option( 'discountdisplayoptions' )['enabled']):?>
-        <script type="text/javascript">
+    <?php if ($config_array = get_option( 'discountdisplayoptions' )):?>
+        <?php if ('1' === $config_array['enabled']):?>
+            <script type="text/javascript">
 
-            <?php       
-            $config_array = get_option( 'discountdisplayoptions' );
-                foreach ($config_array as $key => $value){
+                <?php foreach ($config_array as $key => $value){
                     $config_array[$key] = esc_attr($value);
-                }
-            ?>
+                }?>
 
-            var jwsDDConfigArray = <?php echo json_encode($config_array) ?>;
-            jwsDDConfigArray.currencySymbol = '<?php echo  html_entity_decode(get_woocommerce_currency_symbol()); ?>';
-            jwsDDConfigArray.offString = '<?php echo esc_html__('Off','jwsdiscountdisplay') ?>';
+                var jwsDDConfigArray = <?php echo json_encode($config_array) ?>;
+                jwsDDConfigArray.currencySymbol = '<?php echo  html_entity_decode(get_woocommerce_currency_symbol()); ?>';
+                jwsDDConfigArray.offString = '<?php echo esc_html__('Off','jwsdiscountdisplay') ?>';
 
-        </script>
+            </script>
 
-        <script type="text/javascript" src = "<?php echo plugins_url( 'js/discount-display-setup.js', __FILE__ ); ?>?ver=4.8.1"></script>
+            <script type="text/javascript" src = "<?php echo plugins_url( 'js/discount-display-setup.js', __FILE__ ); ?>?ver=4.8.1"></script>
+        <?php endif; ?>
     <?php endif; ?>
 <?php }
